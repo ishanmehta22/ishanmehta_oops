2e03d585d4470e3f19e4e6dd404466b59791e7b7
ishanmehta22 <128795677+ishanmehta22@users.noreply.github.com>
2023-12-14 21:56:12 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int matOne[3][3],matTwo[3][3],matThree[3][3];
+    int i,j,k,sum=0;
+    cout<<"ENTER 9 ELEMENTS FOR FIRST MATRIX:"<<endl;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            cin>>matOne[i][j];
+        }
+    }
+    cout<<"ENTER 9 ELEMENTS FOR SECOND MATRIX:"<<endl;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            cin>>matTwo[i][j];
+        }
+    }
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            sum=0;
+            for(k=0;k<3;k++){
+                sum=sum+(matOne[i][k]*matTwo[k][j]);
+            }
+            matThree[i][j]=sum;
+        }
+    }
+    cout<<"\n"<<"MULTIPLICATION RESULT : "<<endl;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            cout<<matThree[i][j]<<"\t";
+        }
+        cout<<endl;
+    }    
+    cout<<endl;
+    return 0;
+
+}
